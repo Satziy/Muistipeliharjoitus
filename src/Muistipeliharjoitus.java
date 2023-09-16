607df12133be193582d92b456a129a0119f75600
Satziy <pihlajamaa.satu@gmail.com>
2023-09-16 19:23:45 +0300

    Melkein valmis
    Sain pelin toteutettua ohjeiden mukaan, tosin ehkä lopussa turhan monta for -looppia? Saisi varmaan yksinkertaisemminkin toteutettua..
    Katson vielä saisinko oikeiden ja väärien lukumäärät ja prosenttimäärät loppuun.

--- src/Muistipeliharjoitus.java
+++ src/Muistipeliharjoitus.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Muistipeliharjoitus {
@@ -6,23 +7,29 @@ public class Muistipeliharjoitus {
 	public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);	
 		
-		//Esitellään muistipeli ja peli arpoo sattumanvaraisesti 7 numeroa väliltä 1-5
+		//Esitellään muistipeli ja muuttujat
+						
+		Random random = new Random();
+		int[] randomNumbers = new int[7]; //sattumanvaraisten numeroiden taulukko
+		int[] answer= new int[7];		  //pelaajan arvaamien numeroiden taulukko
+		int i=0;
 		
 		System.out.println("Muistipeli\n*************");
 		System.out.println("Yritä muistaa alla olevat numerot.\nNäet numerot 3 sekunnin ajan.");
 		
-		Random random = new Random();
-		int[] randomNumbers = new int[7];
-		int answer;
-		int i=0;
+		
+		//peli arpoo sattumanvaraisesti 7 numeroa väliltä 1-5
 		
 		for (i=0; i<randomNumbers.length; i++)
+			
 		{
 		randomNumbers[i] = random.nextInt(5);
 		System.out.print(randomNumbers[i] + " ");
 		
 		}
+		
 		System.out.print(" (3 sekunnin jälkeen ruutu tyhjennetään)");
+		
 		
 		//Peliin 3 sekunnin tauko
 		
@@ -43,14 +50,38 @@ public class Muistipeliharjoitus {
 		
 		//Pelaaja syöttää muistamansa numerot
 		
-		for (i=0; i<7; i++)
+		for (i=0; i<answer.length; i++)
 			
 			{
 			System.out.println("Syötä " + (i+1) + "." +  " numero:");
-			answer =Integer.parseInt(in.nextLine());
-			
+			answer[i] =Integer.parseInt(in.nextLine());
 			}
 		
+		//Peli kertoo mitkä olivat pelaajan arvaamat numerot
+		
+		System.out.print("Sinun numerosi olivat: " );
+		
+		for (i=0; i<answer.length; i++)
+			{
+			System.out.print(answer[i]);
+			}	
+		
+		//Peli kertoo mitkä olivat oikeat numerot
+				
+		System.out.print("\nOikeat numerot olivat: ");
+		
+		
+		for (i=0; i<randomNumbers.length; i++)
+			
+		{
+		
+		System.out.print(randomNumbers[i]);
+		
+		}
+		
+		System.out.println("\n\nKiitos pelaamisesta!");
+		
+			
 		
 	}
 
