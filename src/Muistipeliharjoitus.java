21fa3288555a8a80c548e1f754701669c871f083
Satziy <pihlajamaa.satu@gmail.com>
2023-09-16 16:57:17 +0300

    Muistipelin alku
    Muistipeliin tehtya alku. Peli esitellään ja 7 sattumanvaraista numeroa väliltä 1-5 generoituu automaattisesti pelaajalle.

--- src/Muistipeliharjoitus.java
+++ src/Muistipeliharjoitus.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class Muistipeliharjoitus {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Muistipeli\n*************");
+		System.out.println("Yritä muistaa alla olevat numerot.\nNäet numerot 3 sekunnin ajan.");
+		
+		Random random = new Random();
+		int[] randomNumbers = new int[7];
+		
+		for (int i=0; i<randomNumbers.length; i++)
+		{
+		randomNumbers[i] = random.nextInt(5);
+		System.out.print(randomNumbers[i] + " ");
+		
+		}
+		System.out.print(" (3 sekunnin jälkeen ruutu tyhjennetään)");
 	}
 
 }
